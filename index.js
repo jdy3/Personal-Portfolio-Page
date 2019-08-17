a04cde52ec42afc1973a3bbb142855bb7c8e5f3c
Judith Youziel <38296075+jdy3@users.noreply.github.com>
2019-08-17 04:31:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+let text = document.getElementById('text');
 
+let heart = document.getElementById('heart');
+
+function explosion(event) {
+  heart.className = heart.className.replace('beat', 'enable');
+  text.className = text.className.replace('hide', 'reveal');
+}
+
+heart.addEventListener('click', explosion);
