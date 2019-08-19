2a4645a00a91031607f3e7435ebc0e0fca53c166
Judith Youziel <38296075+jdy3@users.noreply.github.com>
2019-08-20 00:05:19 +0100

    resolved animation issue with z-index

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-let text = document.getElementById('text');
+let textP = document.getElementById('textP');
 
 let heart = document.getElementById('heart');
 
-function explosion(event) {
+let arrow = document.getElementById('arrow');
+
+let instruct = document.getElementById('instruct')
+
+function anim(event) {
   heart.className = heart.className.replace('beat', 'enable');
-  text.className = text.className.replace('hide', 'reveal');
+  textP.className = textP.className.replace('hide', 'reveal');
+  arrow.classList.add('hide');
+  instruct.classList.add('hide');
 }
 
-heart.addEventListener('click', explosion);
+heart.addEventListener('click', anim);
